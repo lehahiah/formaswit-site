14442671926dc8563df4e99b9662676a263dbe8b
lehahiah <contact.lehahiah@gmail.com>
2026-01-10 17:02:37 +0100

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,186 @@
+export const SITE_CONFIG = {
+  name: 'FormaSwift',
+  description:
+    'Spécialiste des organismes de formation - Accompagnement personnalisé et solutions concrètes',
+  url: 'https://formaswift.com',
+  ogImage: '/og-image.jpg',
+  links: {
+    youtube: 'https://youtube.com/@formaswift',
+    linkedin: 'https://linkedin.com/company/formaswift',
+    facebook: 'https://facebook.com/formaswift',
+    instagram: 'https://instagram.com/formaswift',
+    twitter: 'https://twitter.com/formaswift',
+  },
+  contact: {
+    email: 'julie@formaswift.fr',
+    emailContact: 'julie@formaswift.fr',
+    phone: '07 66 02 54 51',
+    phoneFormatted: '+33 7 66 02 54 51',
+    address: '310 Rue du Lavoir, 38140 Apprieu',
+    company: 'BOURDAIS JULIE (FormaSwift)',
+    siren: '521 471 474',
+    siret: '521 471 474 00036',
+    tva: 'FR83521471474',
+  },
+  business: {
+    founded: '2020',
+    experience: '15 ans',
+    specialties: [
+      'Spécialiste organismes de formation',
+      'Optimisation des processus',
+      'Qualité et organisation',
+      'Accompagnement personnalisé',
+      'Formation professionnelle',
+      'Certification Qualiopi',
+      'Gestion administrative OF',
+      'Digitalisation des processus',
+    ],
+  },
+};
+
+export const NAVIGATION_ITEMS = [
+  {
+    title: 'Accompagnement',
+    href: '/accompagnement',
+  },
+  {
+    title: 'Qualiopi',
+    href: '/qualiopi',
+  },
+  {
+    title: 'Formations',
+    href: '/formations',
+  },
+  {
+    title: 'Ateliers & Kits',
+    href: '/ateliers-kits',
+  },
+  {
+    title: 'Les Guides Formation',
+    href: '/guides',
+  },
+  {
+    title: 'Audit & Coffee',
+    href: '/audit-coffee',
+  },
+  {
+    title: 'Blog',
+    href: '/blog',
+  },
+];
+
+export const FOOTER_LINKS = {
+  company: [
+    {
+      title: 'Accompagnement',
+      href: '/accompagnement',
+    },
+    {
+      title: 'Qualiopi',
+      href: '/qualiopi',
+    },
+    {
+      title: 'Formations',
+      href: '/formations',
+    },
+    {
+      title: 'Catalogue',
+      href: '/catalogue',
+    },
+    {
+      title: 'Ateliers & Kits',
+      href: '/ateliers-kits',
+    },
+    {
+      title: 'Les Guides Formation',
+      href: '/guides',
+    },
+    {
+      title: 'Audit & Coffee',
+      href: '/audit-coffee',
+    },
+    {
+      title: 'Blog',
+      href: '/blog',
+    },
+    {
+      title: 'Partenaires',
+      href: '/partenaires',
+    },
+  ],
+  legal: [
+    {
+      title: 'Mentions légales',
+      href: '/mentions-legales',
+    },
+    {
+      title: 'CGV',
+      href: '/cgv',
+    },
+    {
+      title: 'Contact',
+      href: '/contact',
+    },
+  ],
+};
+
+export const MAIN_SERVICES = [
+  {
+    title: 'FormaSwift',
+    description:
+      "Accompagnement personnalisé pour votre organisme de formation",
+    href: '/formaswift',
+    image: '/images/formaswift-service.jpg',
+  },
+  {
+    title: 'Les Guides Formation',
+    description:
+      "Guides et formations en ligne prêts à l'emploi",
+    href: '/guides',
+    image: '/images/guides-formation.jpg',
+  },
+  {
+    title: 'Audit & Coffee',
+    description:
+      'Goodies et objets personnalisés pour votre OF',
+    href: '/audit-coffee',
+    image: '/images/audit-coffee.jpg',
+  },
+];
+
+export const METHOD_STEPS = [
+  {
+    id: 1,
+    title: 'Analyser',
+    description:
+      'On définit ensemble les besoins réels et les objectifs mesurables.',
+    icon: 'search',
+  },
+  {
+    id: 2,
+    title: 'Concevoir',
+    description: 'On structure un parcours clair et efficace.',
+    icon: 'palette',
+  },
+  {
+    id: 3,
+    title: 'Déployer',
+    description: 'On met en place les actions et outils adaptés.',
+    icon: 'play',
+  },
+  {
+    id: 4,
+    title: 'Mesurer',
+    description:
+      'On suit les résultats, les indicateurs et la progression.',
+    icon: 'barChart',
+  },
+  {
+    id: 5,
+    title: 'Améliorer',
+    description:
+      'On ajuste en continu pour rester pertinent et efficace.',
+    icon: 'cog',
+  },
+];
+export const CALENDAR_LINK = "https://calendar.app.google/dwfC7F7LMHqXcSsT7";
